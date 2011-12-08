9fbfb418305ed0c1138289fd0f6d230d48d6d0b4
ervin.domazet@gmail.com <ervin.domazet@gmail.com@76d0aa95-d0f1-4c25-2816-875fffa43dbf>
2011-12-08 17:37:50 +0000

    Future Event List STRUCT is modified...

--- Event Driven Simulation using Gpu/src/FutureEventList.h
+++ Event Driven Simulation using Gpu/src/FutureEventList.h
@@ -7,24 +7,28 @@
 
 #ifndef FUTUREEVENTLIST_H_
 #define FUTUREEVENTLIST_H_
-#include <thrust/host_vector.h>
+//#include <thrust/host_vector.h>
+#include <vector>
 
 #include "FutureEvent.h"
 
-typedef thrust::host_vector<FutureEvent> event;
+//typedef thrust::host_vector<FutureEvent> event;
+typedef vector<FutureEvent> event;
 struct FutureEventList  {
-	thrust::host_vector<event> *future_event_list;
+	vector <event> *future_event_list;
 	int size;
 	int max_Delay;
 	int time_increments;
-
 };
-FutureEventList_Constructor(FutureEventList& fue, int Maximum_Delay, int time_increments){
+void FutureEventList_Constructor(FutureEventList& fue, int Maximum_Delay, int time_increments){
 	fue.max_Delay = Maximum_Delay;
 	fue.time_increments = time_increments;
 	fue.size = Maximum_Delay / time_increments + 1;
 
-	fue.future_event_list = new thrust::host_vector<event>(size);
+	//fue.future_event_list = new thrust::host_vector<event>(size);
+	fue.future_event_list = new vector<event>(fue.size);
+
 }
+
 
 #endif /* FUTUREEVENTLIST_H_ */
