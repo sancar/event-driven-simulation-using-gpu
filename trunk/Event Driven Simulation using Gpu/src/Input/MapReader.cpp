a2351211dd132e8c4cf0b0181cedeab1daf2f002
ervin.domazet@gmail.com <ervin.domazet@gmail.com@76d0aa95-d0f1-4c25-2816-875fffa43dbf>
2011-12-08 19:48:31 +0000

    projenin Structli hali

--- trunk/Event Driven Simulation using Gpu/src/Input/MapReader.cpp
+++ trunk/Event Driven Simulation using Gpu/src/Input/MapReader.cpp
@@ -12,10 +12,73 @@
 #include "MapReader.h"
 #include "pugixml.hpp"
 
-#ifndef GATES
-#define GATES
-#include "../Gates/BaseGate.h"
-#endif
+struct BaseGate;
+bool operation(BaseGate & gate, bool* inputs);
+void addGate_Output(BaseGate & gate, BaseGate* newGate);
+void define_and_set_nextSignal(BaseGate & gate,bool signal);
+void define_and_set_signal(BaseGate & gate , bool signal);
+void addGate_Input(BaseGate *gate,BaseGate& newGate);
+void BaseGate_Constructor(BaseGate & gate,int delay,int numberOfGates, int numberOfInputs,string name,string type);
+
+void BaseGate_Constructor(BaseGate & gate,int delay,int numberOfGates, int numberOfInputs,string name,string type){
+	gate._type=type;
+	gate._name = name;
+	gate._currentNumberOfGates_Output = 0;
+	gate._currentNumberOfGates_Input=0;
+	gate._numberOfGates_Output = numberOfGates;
+	gate._defined = false;
+	gate._full_O = false;
+	gate._full_I=false;
+	gate._numOfInputs=numberOfInputs;
+	gate._defined_nextSignal=false;
+	if(numberOfGates!=0){//eger o ise en son output demektir!
+		gate._outputGates = new BaseGate*[gate._numberOfGates_Output];
+	}
+	else
+	{
+		gate._full_O=true;
+		gate._outputGates=NULL;
+	}
+	if(numberOfInputs!=0){//eger 0 ise en bastaki input demektir
+		gate._inputGates=new BaseGate*[gate._numOfInputs];
+	}
+	else
+	{
+		 gate._full_I=true;
+		 gate._inputGates=NULL;
+	}
+	gate._delay = delay;
+}
+void InputVector_Constructor(InputVector& iv  , BaseGate* gate, int time, bool value){
+	iv._ptr_gate = gate;
+	iv._switches_to = value;
+	iv._at_time_unit = time;
+}
+
+
+void addGate_Input(BaseGate *gate,BaseGate& newGate){
+	if(!gate->_full_I){
+		gate->_inputGates[gate->_currentNumberOfGates_Input] = &newGate;
+		gate->_currentNumberOfGates_Input++;
+		if(gate->_currentNumberOfGates_Input == gate->_numOfInputs){
+			gate->_full_I = true;
+		}
+	}else{
+		cerr<< "Error(Input): You are trying add more gates than specified to an Interconnection " << endl;
+	}
+}
+void addGate_Output(BaseGate & gate, BaseGate* newGate){
+	if(!gate._full_O){
+		gate._outputGates[gate._currentNumberOfGates_Output] = newGate;
+		gate._currentNumberOfGates_Output++;
+		addGate_Input(newGate,gate);
+		if(gate._currentNumberOfGates_Output == gate._numberOfGates_Output ){
+			gate._full_O = true;
+		}
+	}else{
+		cerr<< "Error(Output): You are trying add more gates than specified to an Interconnection " << endl;
+	}
+}
 
 using namespace std;
 using namespace pugi;
--- trunk/Event Driven Simulation using Gpu/src/InputVector.h
+++ trunk/Event Driven Simulation using Gpu/src/InputVector.h
@@ -8,7 +8,8 @@
 #ifndef INPUTVECTOR_H_
 #define INPUTVECTOR_H_
 
-#include "Gates/BaseGate.h"
+struct BaseGate;
+
 struct InputVector {
 
 	//InputVector(int, bool);
@@ -16,10 +17,6 @@ struct InputVector {
 	int _at_time_unit;
 	bool _switches_to;
 };
-void InputVector_Constructor(InputVector& iv  , BaseGate* gate, int time, bool value){
-	iv.ptr_gate = gate;
-	iv.switches_to = value;
-	iv.at_time_unit = time;
-}
+
 
 #endif /* INPUTVECTOR_H_ */
