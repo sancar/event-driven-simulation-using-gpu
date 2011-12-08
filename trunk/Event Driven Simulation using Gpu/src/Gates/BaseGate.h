af0af9bdc9ee808232dd29278664f1cb71d5fbfd
ervin.domazet@gmail.com <ervin.domazet@gmail.com@76d0aa95-d0f1-4c25-2816-875fffa43dbf>
2011-12-08 17:26:26 +0000

    Gate'lerde STRUCT yapisina donuldu..

--- trunk/Event Driven Simulation using Gpu/src/Gates/BaseGate.h
+++ trunk/Event Driven Simulation using Gpu/src/Gates/BaseGate.h
@@ -6,34 +6,11 @@
  */
 #ifndef BASEGATE_H_
 #define BASEGATE_H_
-#include <vector>
 #include <string>
-using std::vector;
 using namespace std;
-//typedef vector<BaseGate> event;
-class BaseGate{
-public:
-	BaseGate(int,int,int,string);
-    virtual ~BaseGate();
-	void define_and_set_signal(bool);
-	void define_and_set_nextSignal(bool);
-	void setSignal(bool);
-	void addGate_Output(BaseGate*);
-	void addGate_Input(BaseGate*);
-	bool getSignal();
-	string getName();
-	bool isDefined();
-	bool isDefined_NewSignal();
-	bool getNextSignalValue();
-	virtual bool operation(bool*)=0;
-	int getDelay();
-    // TODO these two for debugging only, can be deleted
-    int getNumberOfGates_Output();
-    int getNumberOfGates_Input();
 
-    BaseGate** getOutputGates();
-    BaseGate** getInputGates();
-protected:
+struct BaseGate{
+    string _type;
     string _name;
 	int _delay;
 	bool _currentOutputSignal;
@@ -48,6 +25,112 @@ protected:
 	BaseGate** _inputGates;
 	BaseGate** _outputGates;
 };
+void addGate_Input(BaseGate *gate,BaseGate& newGate);
+void BaseGate_Constructor(BaseGate & gate,int delay,int numberOfGates, int numberOfInputs,string name,string type){
+	gate._type=type;
+	gate._name = name;
+	gate._currentNumberOfGates_Output = 0;
+	gate._currentNumberOfGates_Input=0;
+	gate._numberOfGates_Output = numberOfGates;
+	gate._defined = false;
+	gate._full_O = false;
+	gate._full_I=false;
+	gate._numOfInputs=numberOfInputs;
+	gate._defined_nextSignal=false;
+	if(numberOfGates!=0){//eger o ise en son output demektir!
+		gate._outputGates = new BaseGate*[gate._numberOfGates_Output];
+	}
+	else
+	{
+		gate._full_O=true;
+		gate._outputGates=NULL;
+	}
+	if(numberOfInputs!=0){//eger 0 ise en bastaki input demektir
+		gate._inputGates=new BaseGate*[gate._numOfInputs];
+	}
+	else
+	{
+		 gate._full_I=true;
+		 gate._inputGates=NULL;
+	}
+	gate._delay = delay;
+}
+void define_and_set_signal(BaseGate & gate , bool signal){
+	gate._defined=true;
+	gate._currentOutputSignal = signal;
+}
+void define_and_set_nextSignal(BaseGate & gate,bool signal){
+	gate._defined_nextSignal=true;
+	gate._next_signalvalue = signal;
+}
 
+void addGate_Output(BaseGate & gate, BaseGate* newGate){
+	if(!gate._full_O){
+		gate._outputGates[gate._currentNumberOfGates_Output] = newGate;
+		gate._currentNumberOfGates_Output++;
+		addGate_Input(newGate,gate);
+		if(gate._currentNumberOfGates_Output == gate._numberOfGates_Output ){
+			gate._full_O = true;
+		}
+	}else{
+		cerr<< "Error(Output): You are trying add more gates than specified to an Interconnection " << endl;
+	}
+}
+void addGate_Input(BaseGate *gate,BaseGate& newGate){
+	if(!gate->_full_I){
+		gate->_inputGates[gate->_currentNumberOfGates_Input] = newGate;
+		gate->_currentNumberOfGates_Input++;
+		if(gate->_currentNumberOfGates_Input == gate->_numOfInputs){
+			gate->_full_I = true;
+		}
+	}else{
+		cerr<< "Error(Input): You are trying add more gates than specified to an Interconnection " << endl;
+	}
+}
 
-#endif /* BASEGATE_H_ */
+bool operation(BaseGate & gate, bool* inputs){
+				if(!gate._type.compare("INPUT")){
+					  return inputs[0];
+				}else if(!gate._type.compare("AND")){
+						bool result = true;
+						for(int i = 0 ; i < gate._numOfInputs ; i++){
+							result = result & inputs[i];
+						}
+						return result;
+				}else if(!gate._type.compare("NAND")){
+					bool result = true;
+						for(int i = 0 ; i < gate._numOfInputs ; i++){
+							result = result & inputs[i];
+						}
+						return !result;
+				}else if(!gate._type.compare("OR")){
+						bool result = false;
+						for(int i = 0 ; i < gate._numOfInputs ; i++){
+							result = result || inputs[i];
+						}
+						return result;
+				}else if(!gate._type.compare("NOR")){
+					bool result = false;
+						for(int i = 0 ; i < gate._numOfInputs ; i++){
+							result = result | inputs[i];
+						}
+						return !result;
+				}else if(!gate._type.compare("XOR")){
+						bool result = false;
+						for(int i = 0 ;  i < gate._numOfInputs ; i++){
+							result = result ^ inputs[i];
+						}
+						return result;
+				}else if(!gate._type.compare("XNOR")){
+						bool result = false;
+						for(int i = 0 ;  i < gate._numOfInputs ; i++){
+							result = result ^ inputs[i];
+						}
+						return !result;
+				}else if(!gate._type.compare("NOT")){
+					return !inputs[0];
+				}else if(!gate._type.compare("FLIPFLOP")){
+					return inputs[0];
+				}
+				return true;
+}
